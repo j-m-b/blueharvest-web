66b5594eea87125f3e05010e33b145928175460c
jmb <jamie@whitepumpkin.com>
2015-10-31 03:29:17 -0400

    debug location web service

--- App_Code/Location.cs
+++ App_Code/Location.cs
@@ -26,27 +26,23 @@ public class Location {
                 cmd.Parameters["@id"].Direction = System.Data.ParameterDirection.Input;
                 cmd.Parameters["@id"].Value = id;
                 // output parameter(s)
-                cmd.Parameters.Add("@id", System.Data.SqlDbType.UniqueIdentifier);
-                cmd.Parameters["@id"].Direction = System.Data.ParameterDirection.Output;
-                cmd.Parameters.Add("@anniversary", System.Data.SqlDbType.DateTime);
-                cmd.Parameters["@anniversary"].Direction = System.Data.ParameterDirection.Output;
-                cmd.Parameters.Add("@password", System.Data.SqlDbType.NVarChar, 255);
-                cmd.Parameters["@password"].Direction = System.Data.ParameterDirection.Output;
-                cmd.Parameters.Add("@salt", System.Data.SqlDbType.UniqueIdentifier);
-                cmd.Parameters["@salt"].Direction = System.Data.ParameterDirection.Output;
-                cmd.Parameters.Add("@email", System.Data.SqlDbType.NVarChar, 50);
-                cmd.Parameters["@email"].Direction = System.Data.ParameterDirection.Output;
-                cmd.Parameters.Add("@active", System.Data.SqlDbType.Bit);
-                cmd.Parameters["@active"].Direction = System.Data.ParameterDirection.Output;
-                cmd.Parameters.Add("@locked", System.Data.SqlDbType.Bit);
-                cmd.Parameters["@locked"].Direction = System.Data.ParameterDirection.Output;
+                cmd.Parameters.Add("@latitude", System.Data.SqlDbType.Decimal, 10);
+                cmd.Parameters["@latitude"].Precision = 10;
+                cmd.Parameters["@latitude"].Scale = 7;
+                cmd.Parameters["@latitude"].Direction = System.Data.ParameterDirection.Output;
+                cmd.Parameters.Add("@longitude", System.Data.SqlDbType.Decimal, 10);
+                cmd.Parameters["@longitude"].Precision = 10;
+                cmd.Parameters["@longitude"].Scale = 7;
+                cmd.Parameters["@longitude"].Direction = System.Data.ParameterDirection.Output;
+                cmd.Parameters.Add("@altitude", System.Data.SqlDbType.Int);
+                cmd.Parameters["@altitude"].Direction = System.Data.ParameterDirection.Output;
                 // open and execute
                 c.Open(); cmd.ExecuteNonQuery();
                 if (!Convert.IsDBNull(cmd.Parameters["@latitude"].Value) && !Convert.IsDBNull(cmd.Parameters["@longitude"].Value)) {
                     this.id = Guid.Parse(cmd.Parameters["@id"].Value.ToString());
-                    this.latitude = (double)cmd.Parameters["@latitude"].Value;
-                    this.latitude = (double)cmd.Parameters["@longitude"].Value;
-                    this.latitude = (int)cmd.Parameters["@altitude"].Value;
+                    this.latitude = Convert.ToDouble(cmd.Parameters["@latitude"].Value);
+                    this.longitude = Convert.ToDouble(cmd.Parameters["@longitude"].Value);
+                    this.altitude = (int)cmd.Parameters["@altitude"].Value;
                 } else { // no result
                     // what to do??
                 }
@@ -68,11 +64,13 @@ public class Location {
                     // input parameter(s)
                     // id = newid()
                     cmd.Parameters.Add("@latitude", System.Data.SqlDbType.Decimal, 10);
-                    cmd.Parameters["@latitude"].Precision = 7;
+                    cmd.Parameters["@latitude"].Precision = 10;
+                    cmd.Parameters["@latitude"].Scale = 7;
                     cmd.Parameters["@latitude"].Direction = System.Data.ParameterDirection.Input;
                     cmd.Parameters["@latitude"].Value = l.latitude;
                     cmd.Parameters.Add("@longitude", System.Data.SqlDbType.Decimal, 10);
-                    cmd.Parameters["@longitude"].Precision = 7;
+                    cmd.Parameters["@longitude"].Precision = 10;
+                    cmd.Parameters["@longitude"].Scale = 7;
                     cmd.Parameters["@longitude"].Direction = System.Data.ParameterDirection.Input;
                     cmd.Parameters["@longitude"].Value = l.longitude;
                     cmd.Parameters.Add("@altitude", System.Data.SqlDbType.Int);
@@ -96,15 +94,20 @@ public class Location {
                new System.Data.SqlClient.SqlConnection(System.Configuration.ConfigurationManager.
                ConnectionStrings["blueharvest-rds"].ConnectionString)) {
                 using (System.Data.SqlClient.SqlCommand cmd = new System.Data.SqlClient.SqlCommand(
-                    "blueharvest.dbo.insertLocation", c)) {
+                    "blueharvest.dbo.updateLocation", c)) {
                     cmd.CommandType = System.Data.CommandType.StoredProcedure;
                     // input parameter(s)
+                    cmd.Parameters.Add("@id", System.Data.SqlDbType.UniqueIdentifier);
+                    cmd.Parameters["@id"].Direction = System.Data.ParameterDirection.Input;
+                    cmd.Parameters["@id"].Value = l.id;
                     cmd.Parameters.Add("@latitude", System.Data.SqlDbType.Decimal, 10);
-                    cmd.Parameters["@latitude"].Precision = 7;
+                    cmd.Parameters["@latitude"].Precision = 10;
+                    cmd.Parameters["@latitude"].Scale = 7;
                     cmd.Parameters["@latitude"].Direction = System.Data.ParameterDirection.Input;
                     cmd.Parameters["@latitude"].Value = l.latitude;
                     cmd.Parameters.Add("@longitude", System.Data.SqlDbType.Decimal, 10);
-                    cmd.Parameters["@longitude"].Precision = 7;
+                    cmd.Parameters["@longitude"].Precision = 10;
+                    cmd.Parameters["@longitude"].Scale = 7;
                     cmd.Parameters["@longitude"].Direction = System.Data.ParameterDirection.Input;
                     cmd.Parameters["@longitude"].Value = l.longitude;
                     cmd.Parameters.Add("@altitude", System.Data.SqlDbType.Int);
@@ -114,7 +117,7 @@ public class Location {
                 }
             }
             return true;
-        } catch { // the insert failed somehow (sql error, ids incorrect, etc.)
+        } catch { // the update failed somehow (sql error, ids incorrect, etc.)
             return false;
         }
     }
